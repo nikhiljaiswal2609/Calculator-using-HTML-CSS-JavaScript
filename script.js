4fbd0f669dd19c315d989209557fe17b222d5351
nikhiljaiswal2609 <nikhiljaiswal2609@gmail.com>
2023-12-20 19:59:34 +0530

    first commit

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button'); 
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
     })
-})
+} 
 
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+} 
